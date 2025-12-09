303121c8438b7083fc3f140c2f8a3bc01915cffa
MinhTri <vodaiminhtri@gmail.com>
2025-12-09 21:26:08 +0700

    are oke

--- utils/mail.js
+++ utils/mail.js
@@ -1,9 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    // port: 465,
-    secure: true,
+   service: "gmail",
     auth: {
       user: 'vodaiminhtri@gmail.com',
       pass: 'ipurbnuloaojneun'
